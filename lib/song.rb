5093790a3451afbe4014fa18664a986743ac252e
Shana Wiltshire <shanawiltshire@gmail.com>
2020-05-02 19:40:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,9 +12,9 @@ class Song
   end
 
   def self.new_by_filename(file_name)
-   @song = file_name.split(" - ")[1]
-   @artist = file_name.split(" - ")[0]
-   new_song = self.new(song)
+   song = file_name.split(" - ")[1]
+   artist = file_name.split(" - ")[0]
+   new_song = self.new({song:song, artist:artist})
    #new_song.artist << @artist
    #new_song.artist
    new_song
