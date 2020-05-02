b67597348f91c5dd7a35b6d781e55e31fff88d8d
Shana Wiltshire <shanawiltshire@gmail.com>
2020-05-02 20:08:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,6 +16,7 @@ class Song
    artist = file_name.split(" - ")[0]
    new_song = self.new(song)
    artist = Artist.new(artist)
+   artist.songs << new_song
    new_song.artist = artist
    new_song
  end
